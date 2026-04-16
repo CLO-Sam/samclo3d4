d24ea21b3b41d2abdbfb8f411d519b24c9646e12
Sam Go <sam@clo3d.com>
2026-04-16 19:36:37 +0900

    Revert "메인 레이아웃"
    This reverts commit 25fc7140ca40215675e6bc8caf9cc249400f26ea.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  compiler: {
-    emotion: true
-  }
 };
 
 export default nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,6 @@
-import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // React Query를 사용하기 위한 기본 엔진을 켜는 작업입니다.
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
