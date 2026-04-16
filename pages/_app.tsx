25fc7140ca40215675e6bc8caf9cc249400f26ea
Sam Go <sam@clo3d.com>
2026-04-16 17:09:22 +0900

    메인 레이아웃

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // React Query를 사용하기 위한 기본 엔진을 켜는 작업입니다.
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
